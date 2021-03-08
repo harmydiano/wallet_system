384f16e7f218ed8014743c0f39f293c3443a80ab
harmydiano <harmylarry20@gmail.com>
2021-03-08 22:59:40 +0100

    fixed docker mongoose connection error

--- setup/database.py
+++ setup/database.py
@@ -5,7 +5,7 @@ class Database:
 
     def __init__(self):
         try:
-            self.db = mongoengine.connect('walletSys', host='localhost:27017')
+            self.db = mongoengine.connect('walletSys', host='fpl_mongodb_1:27017')
             print('db connected')
         except Exception as e:
             print('something went wrong' + e)
